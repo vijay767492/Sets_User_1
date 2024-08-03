d3633c1e77b1644443aefba8ac9410ce919b4a97
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-03 16:21:16 +0530

    Create LinkedHashSets.java

---
+++ LinkedHashSets.java
@@ -0,0 +1,4 @@
+public class LinkedHashSets{
+public static void main(String[]args){
+  // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+}
